a98d2d853b540f07ed7f46f5fdb0de0c293ca298
Olivier <olivier.tille@gmail.com>
2011-11-24 09:27:51 -0500

    cleaning up and style changes

--- gen/com/oliviernt/picturme/R.java
+++ gen/com/oliviernt/picturme/R.java
@@ -10,25 +10,43 @@ package com.oliviernt.picturme;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int black=0x7f040001;
+        public static final int blue=0x7f040005;
+        public static final int brown=0x7f040008;
+        public static final int dark_orchid=0x7f040003;
+        public static final int dark_slate_blue=0x7f040007;
+        public static final int grey=0x7f04000c;
+        public static final int orange_red=0x7f040002;
+        public static final int purple=0x7f04000a;
+        public static final int red=0x7f040009;
+        public static final int siena1=0x7f040004;
+        public static final int teal_blue=0x7f040006;
+        public static final int white=0x7f040000;
+        public static final int yellow=0x7f04000b;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int linearLayout1=0x7f050000;
-        public static final int preview_image=0x7f050004;
-        public static final int process_data_btn=0x7f050003;
-        public static final int select_picture_btn=0x7f050002;
-        public static final int take_picture_btn=0x7f050001;
+        public static final int linearLayout1=0x7f060001;
+        public static final int preview_image=0x7f060006;
+        public static final int process_data_btn=0x7f060005;
+        public static final int select_other_btn=0x7f060004;
+        public static final int select_picture_btn=0x7f060003;
+        public static final int take_picture_btn=0x7f060002;
+        public static final int textView1=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int process_data=0x7f040004;
-        public static final int select_picture_btn=0x7f040002;
-        public static final int take_picture_btn=0x7f040003;
-        public static final int upload_picture=0x7f040005;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int process_data=0x7f050004;
+        public static final int select_new_picture=0x7f050006;
+        public static final int select_picture_btn=0x7f050002;
+        public static final int take_picture_btn=0x7f050003;
+        public static final int upload_picture=0x7f050005;
     }
 }
--- src/com/oliviernt/picturme/picturMeActivity.java
+++ src/com/oliviernt/picturme/picturMeActivity.java
@@ -1,37 +1,17 @@
 package com.oliviernt.picturme;
 
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
-import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.HttpParams;
-import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.JSONTokener;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -40,9 +20,8 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
 import android.provider.MediaStore;
-import lib.Base64;
+import lib.Config;
 
 public class picturMeActivity extends Activity implements OnClickListener {
 
@@ -53,14 +32,8 @@ public class picturMeActivity extends Activity implements OnClickListener {
 	private ImageView preview_image;
 	private Bitmap bmp = null;
 	private String message;
-
 	private JSONObject finalResult;
-
-	ArrayList<NameValuePair> nameValuePairs;
-
-	private static final int CODE_SELECT = 1;
-	private static final int CODE_TAKE = 2;
-	protected final Handler handler = new Handler();
+	private ProgressDialog dialog;
 	protected Runnable runner = new Runnable() {
 		@Override
 		public void run() {
@@ -68,8 +41,13 @@ public class picturMeActivity extends Activity implements OnClickListener {
 		}
 	};
 
-	String TAG = "PICTURLOG";
-	private ProgressDialog dialog;
+	private final String TAG = "PICTURLOG";
+
+	private static final int CODE_SELECT = 1;
+	private static final int CODE_TAKE = 2;
+
+	protected final Handler handler = new Handler();
+	private Button select_new;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -78,18 +56,27 @@ public class picturMeActivity extends Activity implements OnClickListener {
 
 		setContentView(R.layout.main);
 
+		if (!Config.hasNetworkConnection(this)) {
+			message = "No upload possible without a network connection!";
+			Config.toast(this, message);
+		}
+
+		/* Get view elements */
 		select = (Button) this.findViewById(R.id.select_picture_btn);
-		select.setOnClickListener(this);
-
 		take = (Button) this.findViewById(R.id.take_picture_btn);
-		take.setOnClickListener(this);
-		// take.setClickable(false);// disable take button as it doesn't work
-
 		process = (Button) this.findViewById(R.id.process_data_btn);
-		process.setOnClickListener(this);
-		process.setVisibility(View.GONE);
-
+		select_new = (Button) this.findViewById(R.id.select_other_btn);
 		preview_image = (ImageView) this.findViewById(R.id.preview_image);
+
+		/* Set processing button to hidden */
+		process.setVisibility(View.GONE);
+		select_new.setVisibility(View.GONE);
+
+		/* Bind listener to buttons */
+		select.setOnClickListener(this);
+		take.setOnClickListener(this);
+		process.setOnClickListener(this);
+		select_new.setOnClickListener(this);
 	}
 
 	@Override
@@ -107,10 +94,24 @@ public class picturMeActivity extends Activity implements OnClickListener {
 			intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 			startActivityForResult(intent, CODE_TAKE);
 		}
+		
+		if (v == select_new) {
+			select_new.setVisibility(View.GONE);
+			process.setVisibility(View.GONE);
+			take.setVisibility(View.VISIBLE);
+			select.setVisibility(View.VISIBLE);
+		}
 
 		if (v == process) {
 			preview_image.setVisibility(View.GONE);
 			process.setVisibility(View.GONE);
+
+			if (!Config.hasNetworkConnection(this)) {
+				message = "No upload possible without a network connection!";
+				Config.toast(this, message);
+				return;
+			}
+
 			take.setVisibility(View.GONE);
 			select.setVisibility(View.GONE);
 
@@ -119,28 +120,12 @@ public class picturMeActivity extends Activity implements OnClickListener {
 				// upload the image to the server
 				Log.d(TAG, "Uploading the image");
 				Thread t = new Thread() {
-
 					public void run() {
 						try {
-							scaleBmp();
-							ByteArrayOutputStream bos = new ByteArrayOutputStream();
-							bmp.compress(CompressFormat.PNG, 0, bos);
-							byte[] bitmapdata = bos.toByteArray();
+							bmp = Config.scaleBmp(bmp, 500);
 
-							String encoded = Base64.encodeBytes(bitmapdata);
+							HttpResponse response = Config.uploadBitmap(bmp);
 
-							nameValuePairs = new ArrayList<NameValuePair>();
-
-							nameValuePairs.add(new BasicNameValuePair("file",
-									encoded));
-							Log.d(TAG, "Uploading...");
-							String url = "http://www.pictur.me/upload.ajax";
-							HttpClient httpclient = new DefaultHttpClient();
-							HttpPost httppost = new HttpPost(url);
-							httppost.setEntity(new UrlEncodedFormEntity(
-									nameValuePairs));
-							HttpResponse response = httpclient
-									.execute(httppost);
 							BufferedReader reader = new BufferedReader(
 									new InputStreamReader(response.getEntity()
 											.getContent(), "UTF-8"));
@@ -177,9 +162,14 @@ public class picturMeActivity extends Activity implements OnClickListener {
 			try {
 				bmp = MediaStore.Images.Media.getBitmap(
 						this.getContentResolver(), uri);
+				
 				preview_image.setImageBitmap(bmp);
-				preview_image.setVisibility(View.VISIBLE);
+
+				take.setVisibility(View.GONE);
+				select.setVisibility(View.GONE);
+				select_new.setVisibility(View.VISIBLE);
 				process.setVisibility(View.VISIBLE);
+				preview_image.setVisibility(View.VISIBLE);
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 				Log.e(TAG, e.getMessage());
@@ -191,38 +181,21 @@ public class picturMeActivity extends Activity implements OnClickListener {
 		// if an image was made with the camera, get the bitmap data
 		if (requestCode == CODE_TAKE && resultCode == Activity.RESULT_OK) {
 			bmp = (Bitmap) data.getExtras().get("data");
-			scaleBmp();
+			bmp = Config.scaleBmp(bmp);
+			
 			preview_image.setImageBitmap(bmp);
-			preview_image.setVisibility(View.VISIBLE);
+			
+			take.setVisibility(View.GONE);
+			select.setVisibility(View.GONE);
+			select_new.setVisibility(View.VISIBLE);
 			process.setVisibility(View.VISIBLE);
+			preview_image.setVisibility(View.VISIBLE);
 		}
 	}
 
-	public boolean scaleBmp() {
-
-		float bmpWidth = bmp.getWidth();
-		float bmpHeight = bmp.getHeight();
-
-		int width = 500;
-
-		bmpHeight = (bmpHeight / bmpWidth) * 500;
-		int height = (int) bmpHeight;
-
-		if (bmp.getHeight() > bmp.getWidth()) {
-			height = 500;
-			bmpWidth = (bmp.getWidth() / bmp.getHeight()) * 500;
-			width = (int) bmpWidth;
-		}
-
-		if (height > 0 && width > 0) {
-			bmp = Bitmap.createScaledBitmap(bmp, (width), (height), true);
-			preview_image.setImageBitmap(bmp);
-			return true;
-		} else {
-			return false;
-		}
-	}
-
+	/**
+	 * handle the data once the upload is finished
+	 */
 	public void handleData() {
 		Log.d(TAG, "Uploaded!");
 		take.setVisibility(View.VISIBLE);
@@ -237,28 +210,14 @@ public class picturMeActivity extends Activity implements OnClickListener {
 			success = finalResult.getBoolean("success");
 			url += finalResult.getString("url_path");
 		} catch (Exception e) {
+			e.printStackTrace();
+			Log.e(TAG, e.getMessage());
 		}
 		if (success) {
 			message = "Picture was uploaded successfuly!";
 		}
 		dialog.dismiss();
-		Toast.makeText(this, message, Toast.LENGTH_LONG);
-		preview_image.setImageBitmap(getBitmapFromURL(url));
-	}
-
-	public static Bitmap getBitmapFromURL(String src) {
-		try {
-			URL url = new URL(src);
-			HttpURLConnection connection = (HttpURLConnection) url
-					.openConnection();
-			connection.setDoInput(true);
-			connection.connect();
-			InputStream input = connection.getInputStream();
-			Bitmap myBitmap = BitmapFactory.decodeStream(input);
-			return myBitmap;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
+		Config.toast(this, message);
+		preview_image.setImageBitmap(Config.getBitmapFromURL(url));
 	}
 }
